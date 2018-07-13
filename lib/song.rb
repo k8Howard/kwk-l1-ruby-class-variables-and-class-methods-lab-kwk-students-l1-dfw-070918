b9162918f7040df243a7e17aa699625c927eac4a
k8Howard <sweetk8@me.com>
2018-07-13 19:26:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,13 @@ def self.genre_count
   return genre_count
 end
 
+def self.artist_count
+  artist_count = Hash.new(0)
+  @@artists.each do |artist|
+    artist_count[artist] += 1
+  end
+  return artist_count
+end
 
 end
 
