e6699f08de70bdf728010d80e2077bd22822ad96
k8Howard <sweetk8@me.com>
2018-07-13 18:49:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,13 +3,14 @@ class Song
 
 @@count=0
  
- def initialize (artists, genres)
+ def initialize (name, artists, genres)
+  @name = name
   @artists = artists
   @genres = genres
   @@count += 1 
 end
   
-attr_accessor :artists, :genres
+attr_accessor :name, :artists, :genres
   
   def self.count
   @@count
