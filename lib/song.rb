8d4e06ab2e887e9222e768150713d8f21d318c71
k8Howard <sweetk8@me.com>
2018-07-13 19:01:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@ class Song
 
 @@count=0
 @@artists = [] 
+@@genres = []
  def initialize (name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
   @@count += 1 
   @@artists << artist
+  @genres << genre
 end
   
 attr_accessor :name, :artist, :genre
